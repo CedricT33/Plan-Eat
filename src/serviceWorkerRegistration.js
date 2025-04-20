317754bb3a0055dc368a7af152eb2d3b811b4953
unknown <cedric.tressous@gmail.com>
2025-04-20 18:21:48 +0200

    ajout logs pour tests service worker

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,43 +1,53 @@
 const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
+  window.location.hostname === "localhost" ||
     // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
+    window.location.hostname === "[::1]" ||
     // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
 );
 
 let deferredPrompt = null;
 
 export function ajouterEcouteurInstallationApp() {
-  window.addEventListener('beforeinstallprompt', event => {
-    console.log('beforeinstallprompt fired');
+  window.addEventListener("beforeinstallprompt", (event) => {
+    console.log("beforeinstallprompt fired");
     event.preventDefault();
     deferredPrompt = event;
     return false;
-});
+  });
 }
 
-export  function proposerInstallationApp() {
-  console.log('proposerInstallationApp deferredPrompt : ', deferredPrompt);
+export function proposerInstallationApp() {
+  console.log("proposerInstallationApp deferredPrompt : ", deferredPrompt);
   if (deferredPrompt) {
-      deferredPrompt.prompt();
-      deferredPrompt.userChoice.then(choiceResult => {
-          console.log(choiceResult.outcome);
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      console.log(choiceResult.outcome);
 
-          if (choiceResult.outcome === 'dismissed') {
-              console.log('User cancelled installation');
-          }
-          else {
-              console.log('User added to home screen');
-          }
-      });
-      deferredPrompt = null;
+      if (choiceResult.outcome === "dismissed") {
+        console.log("User cancelled installation");
+      } else {
+        console.log("User added to home screen");
+      }
+    });
+    deferredPrompt = null;
   }
 }
 
 export function register(config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  console.log(
+    "----------------> process.env.NODE_ENV : ",
+    process.env.NODE_ENV
+  );
+  console.log(
+    "----------------> serviceWorker in navigator : ",
+    "serviceWorker" in navigator
+  );
+  if (process.env.NODE_ENV === "development" && "serviceWorker" in navigator) {
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    console.log("----------------> publicUrl : ", publicUrl);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -45,7 +55,8 @@ export function register(config) {
       return;
     }
 
-    window.addEventListener('load', () => {
+    window.addEventListener("load", () => {
+      console.log("----------------> load");
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
@@ -56,8 +67,8 @@ export function register(config) {
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
           console.log(
-            'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://cra.link/PWA'
+            "This web app is being served cache-first by a service " +
+              "worker. To learn more, visit https://cra.link/PWA"
           );
         });
       } else {
@@ -69,37 +80,50 @@ export function register(config) {
 }
 
 function registerValidSW(swUrl, config) {
+  console.log("----------------> registerValidSW");
   navigator.serviceWorker
     .register(swUrl)
     .then((registration) => {
+      console.log(
+        "----------------> register OK, registration : ",
+        registration
+      );
       registration.onupdatefound = () => {
+        console.log("---------------------> onupdatefound");
         const installingWorker = registration.installing;
         if (installingWorker == null) {
+          console.log("----------------> installingWorker == null");
           return;
         }
         installingWorker.onstatechange = () => {
-          if (installingWorker.state === 'installed') {
+          console.log("---------------------> onstatechange");
+          if (installingWorker.state === "installed") {
+            console.log(
+              "----------------> installingWorker.state === installed"
+            );
             if (navigator.serviceWorker.controller) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
               console.log(
-                'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://cra.link/PWA.'
+                "New content is available and will be used when all " +
+                  "tabs for this page are closed. See https://cra.link/PWA."
               );
 
               // Execute callback
               if (config && config.onUpdate) {
+                console.log("----------------> config.onUpdate");
                 config.onUpdate(registration);
               }
             } else {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
-              console.log('Content is cached for offline use.');
+              console.log("Content is cached for offline use.");
 
               // Execute callback
               if (config && config.onSuccess) {
+                console.log("----------------> config.onSuccess");
                 config.onSuccess(registration);
               }
             }
@@ -108,24 +132,31 @@ function registerValidSW(swUrl, config) {
       };
     })
     .catch((error) => {
-      console.error('Error during service worker registration:', error);
+      console.error("Error during service worker registration:", error);
     });
 }
 
 function checkValidServiceWorker(swUrl, config) {
   // Check if the service worker can be found. If it can't reload the page.
   fetch(swUrl, {
-    headers: { 'Service-Worker': 'script' },
+    headers: { "Service-Worker": "script" },
   })
     .then((response) => {
+      console.log("----------------> response : ", response);
       // Ensure service worker exists, and that we really are getting a JS file.
-      const contentType = response.headers.get('content-type');
+      const contentType = response.headers.get("content-type");
       if (
         response.status === 404 ||
-        (contentType != null && contentType.indexOf('javascript') === -1)
+        (contentType != null && contentType.indexOf("javascript") === -1)
       ) {
+        console.log("----------------> No service worker found.");
+        console.log("----------------> contentType : ", contentType);
         // No service worker found. Probably a different app. Reload the page.
         navigator.serviceWorker.ready.then((registration) => {
+          console.log(
+            "----------------> navigator.serviceWorker.ready registration : ",
+            registration
+          );
           registration.unregister().then(() => {
             window.location.reload();
           });
@@ -136,12 +167,14 @@ function checkValidServiceWorker(swUrl, config) {
       }
     })
     .catch(() => {
-      console.log('No internet connection found. App is running in offline mode.');
+      console.log(
+        "No internet connection found. App is running in offline mode."
+      );
     });
 }
 
 export function unregister() {
-  if ('serviceWorker' in navigator) {
+  if ("serviceWorker" in navigator) {
     navigator.serviceWorker.ready
       .then((registration) => {
         registration.unregister();
