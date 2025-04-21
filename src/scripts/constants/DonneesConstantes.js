1d4841e717ff50a91b3f612c1ed13dfb2ea602d9
unknown <cedric.tressous@gmail.com>
2025-04-21 16:50:27 +0200

    ajout icone toast

--- src/scripts/constants/DonneesConstantes.js
+++ src/scripts/constants/DonneesConstantes.js
@@ -1,256 +1,249 @@
 export const filtres = [
-    {
-        icone: "sauces",
-        nom: "Sauces",
-        typeDeTri: "categorie",
-        tri: ["sauce"]
-    },
-    {
-        icone: "radis",
-        nom: "Entrées",
-        typeDeTri: "categorie",
-        tri: ["entree"]
-    },
-    {
-        icone: "plat",
-        nom: "Plats",
-        typeDeTri: "categorie",
-        tri: ["plat principal"]
-    },
-    {
-        icone: "gateau",
-        nom: "Desserts",
-        typeDeTri: "categorie",
-        tri: ["dessert"]
-    },
-    {
-        icone: "pizza",
-        nom: "Pizzas",
-        typeDeTri: "categorie",
-        tri: ["pizza"]
-    },
-    {
-        icone: "steak",
-        nom: "Bœuf",
-        typeDeTri: "ingredients.produit",
-        tri: ["boeuf", "bœuf"]
-    },
-    {
-        icone: "saucisse",
-        nom: "Porc",
-        typeDeTri: "ingredients.produit",
-        tri: ["saucisse", "porc"]
-    },
-    {
-        icone: "poulet",
-        nom: "Poulet",
-        typeDeTri: "ingredients.produit",
-        tri: ["poulet", "poule", "poularde", "chapon"]
-    },
-    {
-        icone: "poisson",
-        nom: "Poisson",
-        typeDeTri: "ingredients.produit",
-        tri: ["poisson"]
-    },
-    {
-        icone: "ete",
-        nom: "Eté",
-        typeDeTri: "saison",
-        tri: ["été"]
-    },
-    {
-        icone: "automne",
-        nom: "Automne",
-        typeDeTri: "saison",
-        tri: ["automne"]
-    },
-    {
-        icone: "hiver",
-        nom: "Hiver",
-        typeDeTri: "saison",
-        tri: ["hiver"]
-    },
-    {
-        icone: "printemps",
-        nom: "Printemps",
-        typeDeTri: "saison",
-        tri: ["printemps"]
-    }
-]
+  {
+    icone: "sauces",
+    nom: "Sauces",
+    typeDeTri: "categorie",
+    tri: ["sauce"],
+  },
+  {
+    icone: "radis",
+    nom: "Entrées",
+    typeDeTri: "categorie",
+    tri: ["entree"],
+  },
+  {
+    icone: "plat",
+    nom: "Plats",
+    typeDeTri: "categorie",
+    tri: ["plat principal"],
+  },
+  {
+    icone: "gateau",
+    nom: "Desserts",
+    typeDeTri: "categorie",
+    tri: ["dessert"],
+  },
+  {
+    icone: "pizza",
+    nom: "Pizzas",
+    typeDeTri: "categorie",
+    tri: ["pizza"],
+  },
+  {
+    icone: "steak",
+    nom: "Bœuf",
+    typeDeTri: "ingredients.produit",
+    tri: ["boeuf", "bœuf"],
+  },
+  {
+    icone: "saucisse",
+    nom: "Porc",
+    typeDeTri: "ingredients.produit",
+    tri: ["saucisse", "porc"],
+  },
+  {
+    icone: "poulet",
+    nom: "Poulet",
+    typeDeTri: "ingredients.produit",
+    tri: ["poulet", "poule", "poularde", "chapon"],
+  },
+  {
+    icone: "poisson",
+    nom: "Poisson",
+    typeDeTri: "ingredients.produit",
+    tri: ["poisson"],
+  },
+  {
+    icone: "ete",
+    nom: "Eté",
+    typeDeTri: "saison",
+    tri: ["été"],
+  },
+  {
+    icone: "automne",
+    nom: "Automne",
+    typeDeTri: "saison",
+    tri: ["automne"],
+  },
+  {
+    icone: "hiver",
+    nom: "Hiver",
+    typeDeTri: "saison",
+    tri: ["hiver"],
+  },
+  {
+    icone: "printemps",
+    nom: "Printemps",
+    typeDeTri: "saison",
+    tri: ["printemps"],
+  },
+];
 
 export const categoriesRecettes = [
-    {
-        nom: "sauce",
-        icone: "sauces"
-    },
-    {
-        nom: "entree",
-        icone: "radis"
-    },
-    {
-        nom: "plat principal",
-        icone: "plat"
-    },
-    {
-        nom: "dessert",
-        icone: "gateau"
-    },
-    {
-        nom: "pizza",
-        icone: "pizza"
-    }
-]
+  {
+    nom: "sauce",
+    icone: "sauces",
+  },
+  {
+    nom: "entree",
+    icone: "radis",
+  },
+  {
+    nom: "plat principal",
+    icone: "plat",
+  },
+  {
+    nom: "dessert",
+    icone: "gateau",
+  },
+  {
+    nom: "pizza",
+    icone: "pizza",
+  },
+];
 
 export const rayonsIngredients = [
-    "viandes et poissons",
-    "fruits et légumes",
-    "herbes et épices",
-    "fromages",
-    "produits laitiers et oeufs",
-    "sucre et farine",
-    "huile et vinaigre",
-    "alcools",
-    "autre"
-]
+  "viandes et poissons",
+  "fruits et légumes",
+  "herbes et épices",
+  "fromages",
+  "produits laitiers et oeufs",
+  "sucre et farine",
+  "huile et vinaigre",
+  "alcools",
+  "autre",
+];
 
 export const rayonsProduits = [
-    "hygiène et beauté",
-    "entretien maison",
-    "jardin",
-    "viandes et poissons",
-    "fruits et légumes",
-    "herbes et épices",
-    "fromages",
-    "produits laitiers et oeufs",
-    "sucre et farine",
-    "huile et vinaigre",
-    "alcools",
-    "autre"
-]
+  "hygiène et beauté",
+  "entretien maison",
+  "jardin",
+  "viandes et poissons",
+  "fruits et légumes",
+  "herbes et épices",
+  "fromages",
+  "produits laitiers et oeufs",
+  "sucre et farine",
+  "huile et vinaigre",
+  "alcools",
+  "autre",
+];
 
 export const saisons = [
-    "été",
-    "automne",
-    "hiver",
-    "printemps",
-    "toutes-saisons"
-]
+  "été",
+  "automne",
+  "hiver",
+  "printemps",
+  "toutes-saisons",
+];
 
 export const icones = [
-    "poulet",
-    "steak",
-    "saucisse",
-    "poisson",
-    "crabe",
-    "epice-farine",
-    "fromage",
-    "pomme",
-    "citron",
-    "carotte",
-    "patates",
-    "piment",
-    "citrouille",
-    "betterave",
-    "tomate",
-    "poivron",
-    "champignon",
-    "choux",
-    "salade",
-    "ail",
-    "oignon",
-    "huile",
-    "oeufs",
-    "beurre",
-    "lait",
-    "chocolat"
-]
+  "poulet",
+  "steak",
+  "saucisse",
+  "poisson",
+  "crabe",
+  "epice-farine",
+  "fromage",
+  "pomme",
+  "citron",
+  "carotte",
+  "patates",
+  "piment",
+  "citrouille",
+  "betterave",
+  "tomate",
+  "poivron",
+  "champignon",
+  "choux",
+  "salade",
+  "ail",
+  "oignon",
+  "huile",
+  "oeufs",
+  "beurre",
+  "lait",
+  "chocolat",
+  "toast",
+];
 
 export const iconesProduits = [
-    "plante",
-    "vetement",
-    "chaussure",
-    "soin-dents",
-    "savon",
-    "soda",
-    "alcool",
-    "pain",
-    "poulet",
-    "steak",
-    "saucisse",
-    "poisson",
-    "crabe",
-    "epice-farine",
-    "fromage",
-    "pomme",
-    "citron",
-    "carotte",
-    "patates",
-    "piment",
-    "citrouille",
-    "betterave",
-    "tomate",
-    "poivron",
-    "champignon",
-    "choux",
-    "salade",
-    "ail",
-    "oignon",
-    "huile",
-    "oeufs",
-    "beurre",
-    "lait",
-    "chocolat"
-]
+  "plante",
+  "vetement",
+  "chaussure",
+  "soin-dents",
+  "savon",
+  "soda",
+  "alcool",
+  "pain",
+  "poulet",
+  "steak",
+  "saucisse",
+  "poisson",
+  "crabe",
+  "epice-farine",
+  "fromage",
+  "pomme",
+  "citron",
+  "carotte",
+  "patates",
+  "piment",
+  "citrouille",
+  "betterave",
+  "tomate",
+  "poivron",
+  "champignon",
+  "choux",
+  "salade",
+  "ail",
+  "oignon",
+  "huile",
+  "oeufs",
+  "beurre",
+  "lait",
+  "chocolat",
+  "toast",
+];
 
-export const unitesTemps = [
-    "min",
-    "h",
-    "j"
-]
+export const unitesTemps = ["min", "h", "j"];
 
 export const unites = [
-    {
-        nom: "pièce",
-        raccourci: null
-    },
-    {
-        nom: "gramme",
-        raccourci: "gr"
-    },
-    {
-        nom: "kilo",
-        raccourci: "Kg"
-    },
-    {
-        nom: "centilitre",
-        raccourci: "cL"
-    },
-    {
-        nom: "décilitre",
-        raccourci: "dL"
-    },
-    {
-        nom: "litre",
-        raccourci: "L"
-    },
-    {
-        nom: "cuillère à soupe",
-        raccourci: "cas"
-    },
-    {
-        nom: "cuillère à café",
-        raccourci: "cac"
-    },
-    {
-        nom: "pincée",
-        raccourci: "pcé"
-    }
-]
+  {
+    nom: "pièce",
+    raccourci: null,
+  },
+  {
+    nom: "gramme",
+    raccourci: "gr",
+  },
+  {
+    nom: "kilo",
+    raccourci: "Kg",
+  },
+  {
+    nom: "centilitre",
+    raccourci: "cL",
+  },
+  {
+    nom: "décilitre",
+    raccourci: "dL",
+  },
+  {
+    nom: "litre",
+    raccourci: "L",
+  },
+  {
+    nom: "cuillère à soupe",
+    raccourci: "cas",
+  },
+  {
+    nom: "cuillère à café",
+    raccourci: "cac",
+  },
+  {
+    nom: "pincée",
+    raccourci: "pcé",
+  },
+];
 
-export const motsInvariables = [
-    "pois",
-    "petit pois",
-    "petits pois",
-    "radis"
-]
+export const motsInvariables = ["pois", "petit pois", "petits pois", "radis"];
