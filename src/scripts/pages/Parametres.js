a82ee63344c4c67773058702c1bebe42ecee21f1
unknown <cedric.tressous@gmail.com>
2025-04-21 14:29:20 +0200

    ajout fonctionnalité permettant de sauvegarder et charger les données

--- src/scripts/pages/Parametres.js
+++ src/scripts/pages/Parametres.js
@@ -1,12 +1,32 @@
-import { supprimerToutLocalStorage } from "../services/StorageService";
+import {
+  supprimerToutLocalStorage,
+  sauvegarderDonneesDansFichier,
+  chargerDonneesDepuisFichier,
+  lireFichier,
+} from "../services/StorageService";
 
 export default function Parametres() {
-
-    return (
-        <div className="parametres-container">
-            <div>
-                <button onClick={supprimerToutLocalStorage} className="bouton clic-rouge">RESET</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="parametres-container">
+      <input
+        type="file"
+        id="input-file"
+        onChange={(data) => lireFichier(data.target)}
+      ></input>
+      <div>
+        <button onClick={sauvegarderDonneesDansFichier} className="bouton">
+          SAUVEGARDER
+        </button>
+        <button onClick={chargerDonneesDepuisFichier} className="bouton">
+          CHARGER
+        </button>
+        <button
+          onClick={() => supprimerToutLocalStorage(true)}
+          className="bouton clic-rouge"
+        >
+          RESET
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/scripts/services/StorageService.js
+++ src/scripts/services/StorageService.js
@@ -1,54 +1,77 @@
 import { genererIndexUnique } from "./Util";
 
-    //////////////////////////////////////////////////
-    ///////////////// CONSTANTES /////////////////////
-    //////////////////////////////////////////////////
+//////////////////////////////////////////////////
+///////////////// CONSTANTES /////////////////////
+//////////////////////////////////////////////////
 
 const STORAGE_KEY = "data_plan_eat";
 let DONNEES = null;
 const DONNEES_INIT = {
-    ingredients: [],
-    recettes: [],
-    agenda: [],
-    courses: [],
-    produits: []
-}
+  ingredients: [],
+  recettes: [],
+  agenda: [],
+  courses: [],
+  produits: [],
+};
 
-
-    //////////////////////////////////////////////////
-    ///////////// FONCTIONS PRIVEES //////////////////
-    //////////////////////////////////////////////////
-
+//////////////////////////////////////////////////
+///////////// FONCTIONS PRIVEES //////////////////
+//////////////////////////////////////////////////
 
 function supprimerSiDejaPresent(listeDonnees, key) {
-    if (typeof key === "string") {
-        key = parseInt(key);
+  if (typeof key === "string") {
+    key = parseInt(key);
+  }
+  listeDonnees.forEach((donnee, id) => {
+    if (donnee.key === key) {
+      listeDonnees.splice(id, 1);
     }
-    listeDonnees.forEach((donnee, id) => {
-        if (donnee.key === key) {
-            listeDonnees.splice(id, 1);
-        }
-    })
+  });
 }
 
 function recupererLocalStorage() {
-    const donnees = localStorage.getItem(STORAGE_KEY);
+  const donnees = localStorage.getItem(STORAGE_KEY);
 
-    if (donnees) {
-        DONNEES = JSON.parse(donnees);
-    }
-    else {
-        DONNEES = JSON.parse(JSON.stringify(DONNEES_INIT));
-    }
+  if (donnees) {
+    DONNEES = JSON.parse(donnees);
+  } else {
+    DONNEES = JSON.parse(JSON.stringify(DONNEES_INIT));
+  }
 
-    return DONNEES;
+  return DONNEES;
 }
 
+function uploadFichier(fichier) {
+  const encodedUri = encodeURI(fichier);
+  const link = document.createElement("a");
+  const nomFichier = "plan_eat.json";
+  link.setAttribute("href", encodedUri);
+  link.setAttribute("download", nomFichier);
+  document.body.appendChild(link);
+  link.click();
+  alert("Données sauvegardées !");
+}
 
-    //////////////////////////////////////////////////
-    ///////////// FONCTIONS PUBLIQUES ////////////////
-    //////////////////////////////////////////////////
+function enregistrerDonneesFichier(data) {
+  const donneesFichier = JSON.parse(data);
+  if (
+    donneesFichier &&
+    donneesFichier !== null &&
+    donneesFichier.length !== 0
+  ) {
+    supprimerToutLocalStorage(false);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(donneesFichier));
+    alert("Données chargées !");
+  }
+}
 
+function formaterDonnees(donnees) {
+  return donnees.replaceAll("â¬", "€").replaceAll("Ã©", "é");
+}
+
+//////////////////////////////////////////////////
+///////////// FONCTIONS PUBLIQUES ////////////////
+//////////////////////////////////////////////////
 
 /**
  * Enregistre les données dans le local storage
@@ -57,19 +80,22 @@ function recupererLocalStorage() {
  * exemple : enregistrerLocalStorage({ ingredients: { toto: "toto", key: 1 } });
  */
 export function enregistrerLocalStorage(donneesAEnregistrer) {
-    const donneesLocales = recupererLocalStorage();
+  const donneesLocales = recupererLocalStorage();
 
-    for (const clefdonnee in donneesLocales) {
-        if (clefdonnee === Object.keys(donneesAEnregistrer)[0]) {
-            if (!donneesAEnregistrer[clefdonnee].key) {
-                donneesAEnregistrer[clefdonnee].key = genererIndexUnique();
-            }
-            supprimerSiDejaPresent(donneesLocales[clefdonnee], donneesAEnregistrer[clefdonnee]?.key);
-            donneesLocales[clefdonnee].push(donneesAEnregistrer[clefdonnee]);
-        }
+  for (const clefdonnee in donneesLocales) {
+    if (clefdonnee === Object.keys(donneesAEnregistrer)[0]) {
+      if (!donneesAEnregistrer[clefdonnee].key) {
+        donneesAEnregistrer[clefdonnee].key = genererIndexUnique();
+      }
+      supprimerSiDejaPresent(
+        donneesLocales[clefdonnee],
+        donneesAEnregistrer[clefdonnee]?.key
+      );
+      donneesLocales[clefdonnee].push(donneesAEnregistrer[clefdonnee]);
     }
+  }
 
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(donneesLocales));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(donneesLocales));
 }
 
 /**
@@ -77,16 +103,19 @@ export function enregistrerLocalStorage(donneesAEnregistrer) {
  * @param {Object} donneesASupprimer
  * exemple : supprimerLocalStorage({ ingredients: {key: 1} });
  */
- export function supprimerLocalStorage(donneesASupprimer) {
-    const donneesLocales = recupererLocalStorage();
+export function supprimerLocalStorage(donneesASupprimer) {
+  const donneesLocales = recupererLocalStorage();
 
-    for (const clefdonnee in donneesLocales) {
-        if (clefdonnee === Object.keys(donneesASupprimer)[0]) {
-            supprimerSiDejaPresent(donneesLocales[clefdonnee], donneesASupprimer[clefdonnee]?.key);
-        }
+  for (const clefdonnee in donneesLocales) {
+    if (clefdonnee === Object.keys(donneesASupprimer)[0]) {
+      supprimerSiDejaPresent(
+        donneesLocales[clefdonnee],
+        donneesASupprimer[clefdonnee]?.key
+      );
     }
+  }
 
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(donneesLocales));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(donneesLocales));
 }
 
 /**
@@ -95,16 +124,52 @@ export function enregistrerLocalStorage(donneesAEnregistrer) {
  * @returns {Array} liste de données
  */
 export function recupererDonneesAvecType(type) {
-    recupererLocalStorage();
-    return DONNEES[type];
+  recupererLocalStorage();
+  return DONNEES[type];
 }
 
 /**
  * Suprimme toutes les données de l'app
  */
- export function supprimerToutLocalStorage() {
-    localStorage.removeItem(STORAGE_KEY);
+export function supprimerToutLocalStorage(isAlert) {
+  localStorage.removeItem(STORAGE_KEY);
+  if (isAlert) {
     setTimeout(() => {
-        alert("Données supprimées");
+      alert("Données supprimées");
     }, 100);
+  }
+}
+
+/**
+ * Sauvegarde toutes les données dans un fichier sur le device
+ */
+export function sauvegarderDonneesDansFichier() {
+  let fichierJSON = "data:text/json;charset=utf-8,";
+  const storage = recupererLocalStorage();
+  if (storage && storage.length !== 0) {
+    fichierJSON += JSON.stringify(storage);
+    uploadFichier(fichierJSON);
+  } else {
+    console.log("TODO erreur d'enregistrement des données");
+  }
+}
+
+/**
+ * Charge toutes les données d'un fichier
+ */
+export function chargerDonneesDepuisFichier() {
+  const fileInput = document.querySelector("#input-file");
+  fileInput.click();
+}
+
+/**
+ * Lit les données d'un fichier
+ */
+export async function lireFichier(input) {
+  if (input.files && input.files[0]) {
+    const dataBuffer = await input.files[0].arrayBuffer();
+    const decoder = new TextDecoder();
+    const fichierString = decoder.decode(dataBuffer);
+    enregistrerDonneesFichier(fichierString);
+  }
 }
